964e703f2fe82e05c3b67f64e1438626a9f87d17
Moaegon <mo.agbelusi@biochemistry.org>
2018-11-12 16:55:42 +0000

    board change2

--- sci2/sci2/src/Board.js
+++ sci2/sci2/src/Board.js
@@ -101,7 +101,7 @@ class Board extends Component {
 		return(
 			<div>
 				<div className='navro'><NavHeader NavLinks={NavLinks} /></div>	
-				<Link to="/Standard/Evolution/Science">Evolution</Link>
+				<Link to="/Standard/Evolution/Science">Evolution test</Link>
 				<div className='boardMain'>
 									
 					 <div className='showCase'>{this.grabType()}</div>
